25fef8c33ad6a332235655d82445f6eb819bb5df
Chloé halloin <halloinchloe@gmail.com>
2023-03-14 20:58:45 +0100

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import './App.css';
+import Onepage from '/Page/onepage';
+
+function App() {
+  return (
+    <div className="App">
+      <Onepage />
+    </div>
+  );
+}
+
+export default App;
