6a39d3980cc614a7a01e0a5c1d5042155c2901aa
Chloé halloin <halloinchloe@gmail.com>
2023-03-14 20:58:39 +0100

    Create onepage.tsx

---
+++ Page/onepage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ATrad from '/Component/atrad';
+
+function onepage() {
+  return (
+    <>
+      <div>
+        <ATrad />
+      </div>
+    </>
+  );
+}
+
+export default onepage;
